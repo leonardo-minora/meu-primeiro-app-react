b6a28c3d8a27e5101435b84cd2c4029d38f94507
Leonardo MINORA <leonardo.minora@gmail.com>
2019-12-19 15:57:52 -0300

    react props e state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+function TarefaLista(props) {
+    const titulo = props.title;
+    const dados = props.data;
+    const {title, data} = props;
+    console.log(props)
+    return (
+      <div>
         <p>
-          Meu primeiro App.
+          {title}
         </p>
-      </header>
-    </div>
-  );
+
+        <ul>
+          {data.map((item) => <li>{item}</li>)}
+        </ul>
+      </div>
+    );
+}
+
+const Tarefa = (propriedades) => {
+  return (<div>
+    <p>{propriedades.title}</p>
+    <input type="text" value={propriedades.text} onChange={propriedades.onChange}/>
+    <button type="submit">Criar nova tarefa</button>
+  </div>);
+}
+
+class App extends React.Component {
+  state = {
+    tarefas: [
+      "Aula de componentes",
+      "Aula sobre propriedades e estados",
+      "Outra aula sobre UI"
+    ],
+    text: "Minha nova tarefa"
+  };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = [];
+  // }
+
+  onChangeText(text) {
+    this.setState({text});
+  }
+
+  render() {
+    return (
+      <div>
+        <TarefaLista data={this.state.tarefas} title="Minha lista de tarefas" />
+        <Tarefa 
+          title="Nova tarefa" 
+          text={this.state.text} 
+          onChange={(text) => this.onChangeText(text)} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/comandos.md
@@ -0,0 +1,9 @@
+git init
+git add .
+git config user.name "Minora"
+git config user.email "leonardo.minora@gmail.com"
+git commit -m "meu primeiro app react :D"
+
+https://www.heroku.com/
+
+node_modules/.bin/react-scripts start
